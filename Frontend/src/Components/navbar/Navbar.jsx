fb5f3fb1c1ee40783634c2ae98ecb0fdeab017a6
salove16 <shashank.shalabh909@gmail.com>
2022-08-20 21:13:46 +0530

    done

--- Frontend/src/Components/navbar/Navbar.jsx
+++ Frontend/src/Components/navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { useCookies } from "react-cookie";
+import logo from '../../../assets/logo/brandandbeyond-logo.webp'
 export const Navbar = () => {
   const [cookies, setCookie, removeCookie] = useCookies(["token"]);
   // console.log(cookies)
@@ -9,7 +10,7 @@ export const Navbar = () => {
   return (
     <div style={{alignItems:'center',display:'flex',justifyContent:'space-between',padding:10,paddingRight:'5%',paddingLeft:'5%',boxShadow:' rgba(100, 100, 111, 0.2) 0px 7px 29px 0px'}}>
       <Link  to={"/"}>
-        <img style={{width:125}} src="../../../assets/logo/brandandbeyond-logo.webp" alt="" />
+        <img style={{width:125}} src="logo" alt="" />
       </Link>
 
       {cookies.token ? (
